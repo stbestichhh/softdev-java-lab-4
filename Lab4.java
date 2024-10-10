e9347365420ba849ea174b0f93d8f2221cfe527d
Kiril Yakymchuk <stbestich@gmail.com>
2024-10-10 15:24:28 +0300

    main class

--- Lab4.java
+++ Lab4.java
@@ -1,4 +1,20 @@
 public class Lab4 {
 
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    try {
+      if (args.length < 2) {
+        throw new IlligalArgumentException("Required arguments are missing.\nUsage: java Lab4 'text tot sort' 'target letter'");
+      }
+
+      String textInput = args[0];
+      char targetLetter = args[1];
+
+      Text text = new Text(textInput);
+      text.sortTextByLetterOccurrances(targetLetter);
+
+      System.out.println(text);
+    } catch (Exception e) {
+      System.err.println(e.getMessage());
+    }
+  }
 }
