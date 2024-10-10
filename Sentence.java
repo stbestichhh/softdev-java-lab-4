2f6696beb52bc2087b7b2dbff6f4d0e1f95d5a6f
Kiril Yakymchuk <stbestich@gmail.com>
2024-10-10 15:12:55 +0300

    sort sentence by letter occurrances

---
+++ Sentence.java
@@ -0,0 +1,33 @@
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Represents a sentence in a text
+ */
+public class Sentence {
+  public final List<Object> components;
+
+  public Sentence(String sentence) {
+    components = new ArrayList<>();
+    String[] wordsAndPunctuation = sentence.split("(?<=\\w)(?=\\p{Punct})|\\s+|(?<=\\p{Punct})(?=\\w)");
+    for (String part : wordsAndPunctuation) {
+      if (part.matches("\\p{Punct}")) {
+        components.add(new PunctuationMark(part.charAt(0)));
+      } else {
+        components.add(new Word(part));
+      }
+    }
+  }
+
+  public sortWordsByLetterUccurrances(char targetLetter) {
+    components.sort((obj1, obj2) -> {
+      if (obj1 instanceof Word && obj2 instanceof Word) {
+        Word word1 = (Word) obj1;
+        Word word2 = (Word) obj2;
+        return word1.countLetterOccurrances(targetLetter) - word2.countLetterOccurrances(targetLetter);
+      }
+
+      return 0;
+    });
+  }
+}
